7d8e9e361b4521d143e4215cda9ea5a153ff3c29
ingmarfjolla <ingmar.fjolla@gmail.com>
2025-03-04 13:50:31 -0500

    header cahnge

--- generator/C/include_v2.0/crypto_aead.h
+++ generator/C/include_v2.0/crypto_aead.h
@@ -1,3 +1,6 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
 int crypto_aead_encrypt(unsigned char *c, unsigned long long *clen,
                         const unsigned char *m, unsigned long long mlen,
                         const unsigned char *ad, unsigned long long adlen,
@@ -9,3 +12,6 @@ int crypto_aead_decrypt(unsigned char *m, unsigned long long *mlen,
                         unsigned long long clen, const unsigned char *ad,
                         unsigned long long adlen, const unsigned char *npub,
                         const unsigned char *k);
+#ifdef __cplusplus
+}
+#endif
