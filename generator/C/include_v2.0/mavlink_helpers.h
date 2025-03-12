d101b3c544da0821c75abc2b4416e8a0a35cdb37
ingmarfjolla <ingmar.fjolla@gmail.com>
2025-03-12 14:01:09 -0400

    add some logging

--- generator/C/include_v2.0/mavlink_helpers.h
+++ generator/C/include_v2.0/mavlink_helpers.h
@@ -269,6 +269,7 @@ MAVLINK_HELPER uint16_t mavlink_finalize_message_buffer(mavlink_message_t* msg, 
 		buf[9] = (msg->msgid >> 16) & 0xFF;
 	}
 	bool encrypt = 1;
+	printf("[MAVLink Parser] ENTERED mavlink_finalize_message_buffer function");
 	if (encrypt && msg->msgid!=0){
 		unsigned char key[32] = {0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10,
 			11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
@@ -394,6 +395,7 @@ MAVLINK_HELPER void _mav_finalize_message_chan_send(mavlink_channel_t chan, uint
 
 
 	bool encrypt = 1;
+	printf("[MAVLink Parser] ENTERED _mav_finalize_message_chan_send function");
 	if (encrypt && msgid != 0){
 		unsigned char key[32] = {0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10,
 			11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
@@ -860,6 +862,7 @@ MAVLINK_HELPER uint8_t mavlink_frame_char_buffer(mavlink_message_t* rxmsg,
 				}
 			}
 			bool decrypt = 1;  
+			printf("[MAVLink Parser] ENTERED MAVLINK_PARSE_STATE_GOT_BAD_CRC1 state machine");
 			if (decrypt && rxmsg->msgid != 0) {
 				unsigned char key[32] = {0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10,
 					11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
