316dd6f8fa66e68e2d9e4c29f6d540e0e8ee48de
ingmarfjolla <ingmar.fjolla@gmail.com>
2025-03-13 10:59:34 -0400

    added logging, changed some of the type differences to look into buffer overflow error

--- generator/C/include_v2.0/mavlink_helpers.h
+++ generator/C/include_v2.0/mavlink_helpers.h
@@ -293,12 +293,13 @@ MAVLINK_HELPER uint16_t mavlink_finalize_message_buffer(mavlink_message_t* msg, 
 
 		// encryption here
 		int enc_result = crypto_aead_encrypt(encrypted_packet, &encrypted_length,
-			(const unsigned char*)_MAV_PAYLOAD(msg), length,  
+			_MAV_PAYLOAD(msg), length,  
 			NULL, 0,  
 			NULL, nonce, key);
 		if (enc_result == 0) {
 		// SUCCESS: copy encrypted data back into message payload
 		memcpy(_MAV_PAYLOAD_NON_CONST(msg), encrypted_packet, encrypted_length);
+		printf("Encrypted a packet and replaced OG one !\n");
 		//msg->len = encrypted_length; // Update payload length to encrypted length!
 		} else {
 		printf("Encryption failed!");
@@ -426,17 +427,18 @@ MAVLINK_HELPER void _mav_finalize_message_chan_send(mavlink_channel_t chan, uint
 		// 	&mavlink_system.compid, sizeof(mavlink_system.compid));
 		////
 		
-		unsigned char encrypted_packet[length ];  // Encrypted payload buffer
+		unsigned char encrypted_packet[length];  // Encrypted payload buffer
 		unsigned long long encrypted_length;
 
 		// encryption here
 		int enc_result = crypto_aead_encrypt(encrypted_packet, &encrypted_length,
-			(const unsigned char*)packet, length,  
+			packet, length,  
 			NULL, 0,  
 			NULL, nonce, key);
 		if (enc_result == 0) {
 		// Use memcpy explicitly here, safely copy encrypted payload back into original buffer
-		memcpy((char*)packet, encrypted_packet, encrypted_length);
+		memcpy(packet, encrypted_packet, encrypted_length);
+		printf("Encrypted a packet and replaced OG one !\n");
 		//length = (uint8_t) encrypted_length; 
 		} else {
 		printf("Encryption failed!\n");
